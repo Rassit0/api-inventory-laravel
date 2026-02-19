48a6dfd9f802320bbf9a2de2b263ee147f3c3d4f
Ariel Aramayo <ariel.rr32@gmail.com>
2026-02-19 14:37:25 -0400

    update seed

--- database/seeders/PermissionsDemoSeeder.php
+++ database/seeders/PermissionsDemoSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
@@ -17,62 +18,66 @@ class PermissionsDemoSeeder extends Seeder
         // Reset cached roles and permissions
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        Permission::create(['guard_name' => 'api', 'name' => 'dashboard']);
-        // create permissions
-        Permission::create(['guard_name' => 'api', 'name' => 'register_role']);
-        Permission::create(['guard_name' => 'api', 'name' => 'list_role']);
-        Permission::create(['guard_name' => 'api', 'name' => 'edit_role']);
-        Permission::create(['guard_name' => 'api', 'name' => 'delete_role']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'dashboard']);
+        // firstOrCreate permissions
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'register_role']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'list_role']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'edit_role']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'delete_role']);
 
-        Permission::create(['guard_name' => 'api', 'name' => 'register_user']);
-        Permission::create(['guard_name' => 'api', 'name' => 'list_user']);
-        Permission::create(['guard_name' => 'api', 'name' => 'edit_user']);
-        Permission::create(['guard_name' => 'api', 'name' => 'delete_user']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'register_user']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'list_user']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'edit_user']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'delete_user']);
 
-        Permission::create(['guard_name' => 'api', 'name' => 'settings']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'settings']);
 
-        Permission::create(['guard_name' => 'api', 'name' => 'register_product']);
-        Permission::create(['guard_name' => 'api', 'name' => 'list_product']);
-        Permission::create(['guard_name' => 'api', 'name' => 'edit_product']);
-        Permission::create(['guard_name' => 'api', 'name' => 'delete_product']);
-        Permission::create(['guard_name' => 'api', 'name' => 'show_inventory_product']);
-        Permission::create(['guard_name' => 'api', 'name' => 'show_wallet_price_product']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'register_product']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'list_product']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'edit_product']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'delete_product']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'show_inventory_product']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'show_wallet_price_product']);
 
-        Permission::create(['guard_name' => 'api', 'name' => 'register_client']);
-        Permission::create(['guard_name' => 'api', 'name' => 'list_client']);
-        Permission::create(['guard_name' => 'api', 'name' => 'edit_client']);
-        Permission::create(['guard_name' => 'api', 'name' => 'delete_client']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'register_client']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'list_client']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'edit_client']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'delete_client']);
 
-        Permission::create(['guard_name' => 'api', 'name' => 'register_sale']);
-        Permission::create(['guard_name' => 'api', 'name' => 'list_sale']);
-        Permission::create(['guard_name' => 'api', 'name' => 'edit_sale']);
-        Permission::create(['guard_name' => 'api', 'name' => 'delete_sale']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'register_sale']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'list_sale']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'edit_sale']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'delete_sale']);
 
-        Permission::create(['guard_name' => 'api', 'name' => 'return']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'return']);
 
-        Permission::create(['guard_name' => 'api', 'name' => 'register_purchase']);
-        Permission::create(['guard_name' => 'api', 'name' => 'list_purchase']);
-        Permission::create(['guard_name' => 'api', 'name' => 'edit_purchase']);
-        Permission::create(['guard_name' => 'api', 'name' => 'delete_purchase']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'register_purchase']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'list_purchase']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'edit_purchase']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'delete_purchase']);
 
-        Permission::create(['guard_name' => 'api', 'name' => 'register_transport']);
-        Permission::create(['guard_name' => 'api', 'name' => 'list_transport']);
-        Permission::create(['guard_name' => 'api', 'name' => 'edit_transport']);
-        Permission::create(['guard_name' => 'api', 'name' => 'delete_transport']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'register_transport']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'list_transport']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'edit_transport']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'delete_transport']);
 
-        Permission::create(['guard_name' => 'api', 'name' => 'conversions']);
-        Permission::create(['guard_name' => 'api', 'name' => 'kardex']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'conversions']);
+        Permission::firstOrCreate(['guard_name' => 'api', 'name' => 'kardex']);
 
-        // create roles and assign existing permissions
+        // firstOrCreate roles and assign existing permissions
 
-        $role3 = Role::create(['guard_name' => 'api', 'name' => 'Super-Admin']);
+        $role3 = Role::firstOrCreate(['guard_name' => 'api', 'name' => 'Super-Admin']);
         // gets all permissions via Gate::before rule; see AuthServiceProvider
 
-        $user = \App\Models\User::factory()->create([
-            'name' => 'Super-Admin User',
-            'email' => 'laravest@gmail.com',
-            'password' => bcrypt('12345678')
-        ]);
+       $user = User::firstOrCreate(
+    ['email' => 'laravest@gmail.com'], // condición de búsqueda
+    [
+        'name' => 'Super-Admin User',
+        'password' => bcrypt('12345678')
+    ]
+);
+
+
         $user->assignRole($role3);
     }
 }
